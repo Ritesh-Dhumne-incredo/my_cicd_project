aceac267eff1b8d60a3d470b197129e120548da8
Ritesh Dhumne <153412793+Ritesh-Dhumne@users.noreply.github.com>
2025-11-10 12:13:42 +0530

    2nd commit

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,11 @@
 from src.calculator import add,subtract,mul
 
 def test_add():
-    assert add(2,3)==5,"hello"
+    assert add(2,3)==5
 
 
 def test_subtract():
-    assert subtract(5,3)==2,"hi"
+    assert subtract(5,3)==2
 
 def test_mul():
-    assert mul(5,3)==2,"namaste"
+    assert mul(5,3)==2
